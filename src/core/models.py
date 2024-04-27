5063aec39b46520097476cb7f97418bc11f0c6ae
Atharva <atharva.udavant@cruv.org>
2024-04-28 01:15:11 +0530

    [Atharva] Added : Message sending, storing and retriving functionality

--- src/core/models.py
+++ src/core/models.py
@@ -1,25 +1,35 @@
 from pydantic import BaseModel
 from pymongo import MongoClient
 import os
+from datetime import datetime
 
 USERNAME = os.getenv("mongo_username")
 PASSWORD = os.getenv("mongo_password")
 
 
 client = MongoClient(
-    f"mongodb+srv://{USERNAME}:{PASSWORD}@maindb.ennpbrl.mongodb.net/?retryWrites=true&w=majority&appName=MainDB"
+    f"mongodb+srv://manuloco:manuloco123@maindb.ennpbrl.mongodb.net/?retryWrites=true&w=majority&appName=MainDB"
 )
 db = client["link_irc"]
 sessions_db = db["sessions"]
 users_db = db["users"]
 messages_db = db["messages"]
 
+class Message(BaseModel):
+    content: str
+    userid: str
+    createdat: datetime
 
 class Session(BaseModel):
     sessionid: int
+    adminid: str
+    memberid: str
+    messages: [Message]
     password: str
-
 
 class User(BaseModel):
     username: str
     password: str
+
+
+    
--- src/core/server.py
+++ src/core/server.py
@@ -4,7 +4,7 @@ from fastapi import HTTPException, WebSocket
 from fastapi.responses import JSONResponse
 
 from core.utils import generate_client_id
-from core.models import Session, User, sessions_db, users_db, messages_db
+from core.models import Session, User, sessions_db, users_db, messages_db, Message
 
 
 app = create_app()
@@ -15,6 +15,7 @@ clients = {}
 async def auth_user(user: User):
     user_dict = dict(user)
     cursor = users_db.find_one(user_dict)
+    print(cursor)
     if cursor is None:
         raise HTTPException(status_code=404, detail="User not found")
     return json_util.dumps(cursor)
@@ -40,6 +41,38 @@ async def create_session(session: Session):
     sessions_db.insert_one(session_dict)
     return json_util.dumps(session_dict)
 
+@app.post('/send-message')
+async def send_message(message: Message, session_id: str, password:str):
+    message_dict = dict(message)
+    # Find the session by session_id
+    session = sessions_db.find_one({"sessionid": session_id,"password":password})
+    if session is None:
+        raise HTTPException(status_code=404, detail="Session not found")
+    
+    # Append the new message to the session's messages list
+    if 'messages' in session:
+        session['messages'].append(message_dict)
+    else:
+        session['messages'] = [message_dict]
+    
+    # Update the session document in the database
+    sessions_db.update_one({"sessionid": session_id}, {"$set": {"messages": session['messages']}})
+    
+    return JSONResponse(content=message_dict, media_type="application/json")
+
+
+@app.get('/get-all-messages')
+async def get_all_messages(session_id: str, user_id: str, password:str):
+    # Find the session by session_id
+    session = sessions_db.find_one({"sessionid": session_id,"password": password})
+    if session is None:
+        raise HTTPException(status_code=404, detail="Session not found")
+
+    if user_id == session['adminid'] or user_id == session['memberid']:
+        return JSONResponse(content=session['messages'], media_type="application/json")
+    else:
+        raise HTTPException(status_code=403, detail="User not authorized to view messages")
+
 
 # @app.post("/auth-session")
 # async def auth_session(session: Session):
@@ -50,22 +83,22 @@ async def create_session(session: Session):
 #     return json_util.dumps(cursor)
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    try:
-        await websocket.accept()
-        client_id = generate_client_id()
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     try:
+#         await websocket.accept()
+#         client_id = generate_client_id()
 
-        clients[client_id] = websocket
-        print(f"Client with ID {client_id} connected")
-        while True:
-            data = await websocket.receive_text()
-            response = f"Message text was: {data}"
-            print(response)
-            await websocket.send_text(response)
+#         clients[client_id] = websocket
+#         print(f"Client with ID {client_id} connected")
+#         while True:
+#             data = await websocket.receive_text()
+#             response = f"Message text was: {data}"
+#             print(response)
+#             await websocket.send_text(response)
 
-    except Exception as e:
-        print(e)
-        await websocket.close()
-        print("Connection closed")
-        del clients[client_id]
+#     except Exception as e:
+#         print(e)
+#         await websocket.close()
+#         print("Connection closed")
+#         del clients[client_id]
