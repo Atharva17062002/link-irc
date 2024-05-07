c56faadd1a5b008082a89cbd7b404e6dd910c18c
locobuzz-atharva <atharva.udavant@locobuzz.com>
2024-05-07 12:28:20 +0530

    Asdfghjkl

--- client.py
+++ client.py
@@ -1,14 +1,64 @@
-# client.py
-import asyncio
-import websockets
+import requests as http
+import json
+import threading, os
+from time import sleep
 
-async def main():
-    async with websockets.connect('ws://localhost:8000/ws') as websocket:
-        while True:
-            message = input("Enter message: ")
-            await websocket.send(message)
-            response = await websocket.recv()
-            print(f"Received: {response}")
+BASE_URL = "https://foxhound-premium-antelope.ngrok-free.app:80"
+LOGGED_IN_USER = None
+USERNAME = None
 
-asyncio.run(main())
 
+def login():
+    global LOGGED_IN_USER
+    # username = input("Enter username: ")
+    # password = input("Enter password: ")      
+    username = "ruthking17"
+    password = "1122"
+    try:
+        response = http.post(
+            f"{BASE_URL}/auth-user",
+            json={"username": username, "password": password},
+        )
+        return json.loads(response.json())
+
+    except Exception as e:
+        print(e)
+
+
+USERNAME = login().get("username")
+LOGGED_IN_USER = login().get("_id").get("$oid")
+print(USERNAME)
+print(LOGGED_IN_USER)
+
+
+def session(session_id):
+    threading.Thread(target=fetch_messages).start()
+
+    while True:
+        message = input("Enter message: ")
+        try:
+            response = http.post(
+                f"{BASE_URL}/send-message/{session_id}",
+                json={"content": message, "userid": str(USERNAME)},
+            )
+            print(response.json())
+        except Exception as e:
+            print(e)
+
+
+def fetch_messages():
+    while True:
+        try:
+            response = http.get(f"{BASE_URL}/get-last-messages/888")
+            messages = response.json()
+            # clear the screen
+            print("\033[H\033[J")
+            for message in messages:
+                print(f"{message.get('userid', '')}> / {message.get('content', '')}")
+            sleep(5)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    session(888)
--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-# server.py
-from fastapi import FastAPI, WebSocket
+# # server.py
+# from fastapi import FastAPI, WebSocket
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     await websocket.accept()
+#     while True:
+#         data = await websocket.receive_text()
+#         await websocket.send_text(f"Message text was: {data}")
+print("h")
